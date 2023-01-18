51f3acc87278419f296008f3fb12cf5d7892e0ab
Camille Gauthier <camille.a.gauthier@usherbrooke.ca>
2023-01-18 11:51:52 -0500

    Ajout de la balise Google

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
+      <meta name="google-site-verification" content="55c7DPLmzBSxW2MjmabWAM5mTuLXiJ0hfkjE_PgPbbU" />
       <Title>Hello World</Title>
       <h1>Hello world!</h1>
       <Counter />
