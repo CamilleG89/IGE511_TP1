a84c4598c7a1fea5ac4c4c7284264a5823b6af37
Camille Gauthier <camille.a.gauthier@usherbrooke.ca>
2023-01-18 11:59:29 -0500

    changé emplacement balise meta

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,8 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <meta name="google-site-verification" content="55c7DPLmzBSxW2MjmabWAM5mTuLXiJ0hfkjE_PgPbbU" />
       <Title>Hello World</Title>
+      <meta name="google-site-verification" content="55c7DPLmzBSxW2MjmabWAM5mTuLXiJ0hfkjE_PgPbbU" />
       <h1>Hello world!</h1>
       <Counter />
       <p>
